ac1ffcf5a9cf9eeb4a417787c805739b942ddf08
rosagabriela <rosagabrielapina@gmail.com>
2026-09-10 13:43:02 -0700

    added function for division

--- test/test_potrocal.py
+++ test/test_potrocal.py
@@ -1,4 +1,7 @@
-from addition import addition
+from addition import addition, division
 
 def test_addition():
     assert addition(4,6) == 10
+
+def test_division():
+    assert addition(10, 5) == 15
